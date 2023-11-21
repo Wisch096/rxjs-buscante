4596be1582e2720164d7a7a81e0abf3136e36f5a
Wisch096 <wisch.pg@outlook.com>
2023-11-21 16:58:39 -0300

    Improvement: refatoração no código

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -12,6 +14,8 @@ export class ListaLivrosComponent implements OnDestroy {
   listaLivros: [];
   campoBusca: string = ''
   subscription: Subscription
+  livro: Livro
+
 
   constructor(private service: LivroService) { }
   
@@ -23,6 +27,12 @@ export class ListaLivrosComponent implements OnDestroy {
     });
   }
   
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
