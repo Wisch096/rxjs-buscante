490d8467891a7bb0e2f3406671dcf41892cb6865
Wisch096 <wisch.pg@outlook.com>
2023-12-12 09:02:28 -0300

    Improvement: add debouceTime na pesquisa.

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { filter, map, switchMap } from 'rxjs';
+import { debounceTime, filter, map, switchMap } from 'rxjs';
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
+
 export class ListaLivrosComponent {
 
   campoBusca = new FormControl()
@@ -18,6 +21,7 @@ export class ListaLivrosComponent {
 
   livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
+      debounceTime(PAUSA),
       filter((valorDigitado) => valorDigitado.length >= 3),
       switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
       map((items) => this.livrosResultadoParaLivros(items))
